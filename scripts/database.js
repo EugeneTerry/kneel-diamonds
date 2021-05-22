14671d5881a5b9bfe7cb93c8e2bd1f1dd0441f7e
EugeneTerry <eugene.terry73@gmail.com>
2021-05-22 13:42:38 -0500

    set my setters

--- scripts/database.js
+++ scripts/database.js
@@ -6,6 +6,8 @@
 
 */
 const database = {
+    orderBuilder: {},
+
     styles: [
         { id: 1, style: "Classic", price: 500 },
         { id: 2, style: "Modern", price: 710 },
@@ -34,6 +36,7 @@ const database = {
             timestamp: 1614659931693
         }
     ]
+    
 }
 
 export const getMetals = () => {
@@ -49,4 +52,17 @@ export const getStyles = () => {
 }
 export const getOrders = () => {
     return database.customOrders.map(customOrder => ({...customOrder}));
+}
+
+//Now you need to export functions whose responsibility is to set state.
+export const setMetal = (id) => {
+    database.orderBuilder.metalId = id
+}
+
+export const setSize = (id) => {
+    database.orderBuilder.sizeId = id
+}
+
+export const setStyle = (id) => {
+    database.orderBuilder.styleId = id
 }
